0ff8384314f76a4b883bb9bfbf3ff2b6e22372ae
paolo_roberto_punzo <robertopunzo97@icloud.com>
2025-06-23 10:49:19 +0200

    add: shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,40 +10,101 @@ const config: Config = {
     './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        tiffanyGreen: '#9D0371',
-        violetRose: '#00BAA4'
-      },
-      fontFamily: {
-        sans: ['var(--font-poppins)']
-      },
-      keyframes: {
-        fadeIn: {
-          from: { opacity: '0' },
-          to: { opacity: '1' }
-        },
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' }
-        },
-        blink: {
-          '0%': { opacity: '0.2' },
-          '20%': { opacity: '1' },
-          '100%': { opacity: '0.2' }
-        }
-      },
-      animation: {
-        fadeIn: 'fadeIn .3s ease-in-out',
-        carousel: 'marquee 60s linear infinite',
-        blink: 'blink 1.4s both infinite'
-      }
-    }
+  	extend: {
+  		colors: {
+  			tiffanyGreen: '#9D0371',
+  			violetRose: '#00BAA4',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: [
+  				'var(--font-poppins)'
+  			]
+  		},
+  		keyframes: {
+  			fadeIn: {
+  				from: {
+  					opacity: '0'
+  				},
+  				to: {
+  					opacity: '1'
+  				}
+  			},
+  			marquee: {
+  				'0%': {
+  					transform: 'translateX(0%)'
+  				},
+  				'100%': {
+  					transform: 'translateX(-100%)'
+  				}
+  			},
+  			blink: {
+  				'0%': {
+  					opacity: '0.2'
+  				},
+  				'20%': {
+  					opacity: '1'
+  				},
+  				'100%': {
+  					opacity: '0.2'
+  				}
+  			}
+  		},
+  		animation: {
+  			fadeIn: 'fadeIn .3s ease-in-out',
+  			carousel: 'marquee 60s linear infinite',
+  			blink: 'blink 1.4s both infinite'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   future: {
     hoverOnlyWhenSupported: true
   },
-  darkMode: 'class',
+  darkMode: ['class', 'class'],
   plugins: [
     require('@tailwindcss/container-queries'),
     require('@tailwindcss/typography'),
@@ -61,8 +122,9 @@ const config: Config = {
         }
       );
     }),
-    heroui()
-  ]
+    heroui(),
+      require("tailwindcss-animate")
+]
 };
 
 export default config;
