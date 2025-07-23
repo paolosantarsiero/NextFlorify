93ee10de5a25670224e51a4466b89569aae33692
paolosantarsiero <paolosantarsiero9@gmail.com>
2025-07-23 22:59:52 +0200

    fix: carousel result

--- app/test/carouseldemo.tsx
+++ app/test/carouseldemo.tsx
@@ -1,13 +1,11 @@
-import * as React from "react"
-
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+  CarouselPrevious
+} from '@/components/ui/carousel';
 
 export function CarouselDemo() {
   return (
@@ -28,5 +26,5 @@ export function CarouselDemo() {
       <CarouselPrevious />
       <CarouselNext />
     </Carousel>
-  )
+  );
 }
