24a731f0ac3694920525c7a6c0caa54498ecf124
paolosantarsiero <paolosantarsiero9@gmail.com>
2025-08-05 02:06:48 +0200

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,16 @@ import { Navbar } from '@/components/layout/navbar/NavBar';
 import { NextAuthProvider } from 'components/next-session-provider';
 import { ReactQueryProvider } from 'components/react-query-provider';
 import { Toaster } from 'components/ui/sonner';
-import { ensureStartsWith } from 'lib/utils';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import { Poppins } from 'next/font/google';
 import { ReactNode } from 'react';
 import './globals.css';
 
-const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
+const { SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
-const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
-const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
@@ -26,15 +23,7 @@ export const metadata = {
   robots: {
     follow: true,
     index: true
-  },
-  ...(twitterCreator &&
-    twitterSite && {
-      twitter: {
-        card: 'summary_large_image',
-        creator: twitterCreator,
-        site: twitterSite
-      }
-    })
+  }
 };
 
 const poppins = Poppins({
@@ -55,7 +44,6 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
             <NextAuthProvider>
               <Navbar />
               <FlowersFooter />
-
               <main className="h-dvh w-screen flex flex-col overflow-hidden text-foreground relative z-10">
                 {children}
                 <Toaster closeButton />
