1c5d65c8bf69ea4049ee4c70e0704cd3d10773d0
paolosantarsiero <paolosantarsiero9@gmail.com>
2025-12-04 15:45:12 +0100

    fix: about page

--- __feats/about/AboutPage.tsx
+++ __feats/about/AboutPage.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import { useCssAnimationStore } from '@/__store/cssAnimationsStore';
+import { FLOWER_ANIMATION_NAME, FlowerAnimationStates } from '@/__types/animations/flower';
 import { useTranslations } from 'next-intl';
+import { useEffect } from 'react';
 import regione from '../../assets/images/regione.png';
 
 export default function AboutPage() {
   const t = useTranslations('AboutPage');
+
+  const { setComponentState } = useCssAnimationStore();
+
+  useEffect(() => {
+    setComponentState(FLOWER_ANIMATION_NAME, FlowerAnimationStates.HIDDEN);
+    return () => {
+      setComponentState(FLOWER_ANIMATION_NAME, FlowerAnimationStates.LOADING_STATIC);
+    };
+  }, []);
+
   return (
     <section className="flex flex-col h-screen w-full items-center mt-[80px] overflow-y-scroll">
       <div className="flex flex-col gap-4 w-full max-w-3xl items-center px-3 py-6">
