28c935269573ec3b9cc4014489a1672944a8326e
paolosantarsiero <paolosantarsiero9@gmail.com>
2025-12-28 20:03:43 +0100

    fix: woocommerce client

--- lib/woocomerce/woocommerce.ts
+++ lib/woocomerce/woocommerce.ts
@@ -4,8 +4,8 @@ const option: WooRestApiOptions = {
   url: process.env.WOOCOMMERCE_URL ?? 'http://wordpress.localhost',
   consumerKey: process.env.WOOCOMMERCE_CONSUMER_KEY ?? '',
   consumerSecret: process.env.WOOCOMMERCE_CONSUMER_SECRET ?? '',
-  isHttps: false,
+  isHttps: process.env.WOOCOMMERCE_URL?.startsWith('https') ?? false,
   version: 'wc/v3',
-  queryStringAuth: false // Force Basic Authentication as query string true and using under
+  queryStringAuth: process.env.WOOCOMMERCE_URL?.startsWith('https') ?? false // Force Basic Authentication as query string true and using under
 };
 export const woocommerce = new WooCommerceRestApi(option);
